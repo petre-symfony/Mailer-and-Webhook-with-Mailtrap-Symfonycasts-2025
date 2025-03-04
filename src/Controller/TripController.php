1aed546d980c0537888788f76cbd4d7fdffe3ee5
petrero <petrero@gmail.com>
2025-03-04 09:04:38 +0200

    3.1 Better Email
     I think you, me, anyone that's ever received an email, can agree that our first email stinks. It doesn't provide any value. Let's improve it!
    
     Address Object
     - First, we can add a name to the email. This will show up in most email clients instead of just the email address: it just looks smoother. Wrap the from with new Address(), the one from Symfony\Component\Mime. The first argument is the email, and the second is the name - how about Universal Travel
    
     We can also wrap the to with new Address(). and pass $customer->getName() for the name
    
     For the subject, add the trip name: 'Booking Confirmation for ' . $trip->getName()
    
     For the text body. We could inline all the text right here. That would get ugly, so let's use Twig! We need a template. In templates/, add a new email/ directory and inside, create a new file: booking_confirmation.txt.twig. Twig can be used for any text format, not just html. A good practice is to include the format - .html or .txt - in the filename. But Twig doesn't care about the that - it's just to satisfy our human brains. We'll return to this file in a second.

--- src/Controller/TripController.php
+++ src/Controller/TripController.php
@@ -13,6 +13,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
 use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -45,9 +46,9 @@ final class TripController extends AbstractController {
 			$em->flush();
 
 			$email = (new Email())
-				->from('info@universal-travel.com')
-				->to($customer->getEmail())
-				->subject('Booking Confirmation')
+				->from(new Address('info@universal-travel.com', 'Universal Travel'))
+				->to(new Address($customer->getEmail(), $customer->getName()))
+				->subject('Booking Confirmation for ', $trip->getName())
 				->text('Your booking has been confirmed')
 			;
 
