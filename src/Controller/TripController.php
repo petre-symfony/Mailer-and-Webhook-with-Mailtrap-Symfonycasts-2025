f21536e71414d4de1b9cbd5a0b629b2aa0a535a6
petrero <petrero@gmail.com>
2025-03-06 12:15:36 +0200

    9.5 Reply-To  - One more detail to make this completely airtight (like most of our ships).
     Imagine a contact form where the user fills their name, email, and a message. This fires off an email with these details to your support team. In their email clients, it'd be nice if, when they hit reply, it goes to the email from the form - not your "global from".
    
     You might think that you should set the from address to the user's email. But that won't work, as we're not authorized to send emails on behalf of that user. More on email security soon.
    
     Fortunately, there's a special email header called Reply-To for just this scenario. When building your email, set it with ->replyTo() and pass the user's email address.

--- src/Controller/TripController.php
+++ src/Controller/TripController.php
@@ -52,6 +52,7 @@ final class TripController extends AbstractController {
 
 			$email = (new TemplatedEmail())
 				->to(new Address($customer->getEmail(), $customer->getName()))
+				->replyTo($user->email)
 				->subject('Booking Confirmation for ', $trip->getName())
 				->attachFromPath($termsPath, 'Terms of service.pdf')
 				->htmlTemplate('email/booking_confirmation.html.twig')
