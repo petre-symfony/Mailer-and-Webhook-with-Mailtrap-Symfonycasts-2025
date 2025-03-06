47d7bfa08c3e457b80b5ac7e0454081d3280bb0c
petrero <petrero@gmail.com>
2025-03-06 12:12:55 +0200

    9.4 Back in TripController::show(), remove the ->from() for the email.
     Time to test this! In our app, book a trip and check Mailtrap for the email. Drumroll... the from is set correctly! Our listener works! I never doubted us.

--- src/Controller/TripController.php
+++ src/Controller/TripController.php
@@ -51,7 +51,6 @@ final class TripController extends AbstractController {
 			$em->flush();
 
 			$email = (new TemplatedEmail())
-				->from(new Address('info@universal-travel.com', 'Universal Travel'))
 				->to(new Address($customer->getEmail(), $customer->getName()))
 				->subject('Booking Confirmation for ', $trip->getName())
 				->attachFromPath($termsPath, 'Terms of service.pdf')
