e4c1082258a40f2679a46c1782626b6961657d87
petrero <petrero@gmail.com>
2025-03-06 12:06:39 +0200

    9.3 MessageEvent Listener  - Open the new class src/EventListener/GlobalFromEmailListener.php. Add a constructor with a private string  argument and an #[Autowire] attribute with our parameter name: %global_from_email%
     Down here, the #[AsEventListener] attribute is what marks this method as an event listener. We can actually remove this event argument - it'll be inferred from the method argument's type-hint: MessageEvent
    
     Inside, first grab the message from the event: $message = $event->getMessage()
    
     Jump into the getMessage() method to see what it returns. RawMessage... jump into this and look at what classes extend it. TemplatedEmail! Perfect!
    
     Back in our listener, write if (! $message instanceof TemplatedEmail), and inside, return;
    
     This will likely never be the case, but it's good practice to double-check. Plus, it helps our IDE know that  is a TemplatedEmail now.
    
     It's possible that an email might still set its own from address. In this case, we don't want to override it. So, add a guard clause: if ($message->getFrom()), return;
    
     Now, we can set the global from: $message->from($this->fromEmail)
    
     Perfect!

--- src/EventListener/GlobalFromEmailListener.php
+++ src/EventListener/GlobalFromEmailListener.php
@@ -2,12 +2,31 @@
 
 namespace App\EventListener;
 
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 use Symfony\Component\Mailer\Event\MessageEvent;
 
 final class GlobalFromEmailListener {
-	#[AsEventListener(event: MessageEvent::class)]
+	public function __construct(
+		#[Autowire('%global_from_emails%')]
+		private string $fromEmail
+	) {
+		
+	}
+
+	#[AsEventListener]
 	public function onMessageEvent(MessageEvent $event): void {
-		// ...
+		$message = $event->getMessage();
+
+		if (!$message instanceof TemplatedEmail) {
+			return;
+		}
+
+		if ($message->getFrom()) {
+			return;
+		}
+
+		$message->from($this->fromEmail);
 	}
 }
