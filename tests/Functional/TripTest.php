479699b7799e27db07cc2b4d84d5164cf80eee84
petrero <petrero@gmail.com>
2025-05-02 09:13:10 +0300

    14.1 Emails Assertions in Functional Tests
     Okay, testing time! If you've explored the codebase a bit, you may have noticed that someone (it could've been anyone... but probably a Canadian) snuck some tests into our tests/Functional/ directory. Do these pass? Idk! Let's find out!

--- tests/Functional/TripTest.php
+++ tests/Functional/TripTest.php
@@ -8,27 +8,24 @@ use Zenstruck\Browser\Test\HasBrowser;
 use Zenstruck\Foundry\Test\Factories;
 use Zenstruck\Foundry\Test\ResetDatabase;
 
-class TripTest extends KernelTestCase
-{
-    use ResetDatabase, Factories, HasBrowser;
+class TripTest extends KernelTestCase {
+	use ResetDatabase, Factories, HasBrowser;
 
-    public function testViewTrips(): void
-    {
-        TripFactory::createOne([
-            'name' => 'Visit Mars',
-            'slug' => 'mars',
-            'tagLine' => 'The red planet',
-        ]);
+	public function testViewTrips(): void {
+		TripFactory::createOne([
+			'name' => 'Visit Mars',
+			'slug' => 'mars',
+			'tagLine' => 'The red planet',
+		]);
 
-        $this->browser()
-            ->visit('/')
-            ->assertSuccessful()
-            ->assertSee('Visit Mars')
-            ->assertSee('The red planet')
-            ->click('Visit Mars')
-            ->assertOn('/trip/mars')
-            ->assertSeeIn('h1', 'Visit Mars')
-            ->assertSee('The red planet')
-        ;
-    }
+		$this->browser()
+			->visit('/')
+			->assertSuccessful()
+			->assertSee('Visit Mars')
+			->assertSee('The red planet')
+			->click('Visit Mars')
+			->assertOn('/trip/mars')
+			->assertSeeIn('h1', 'Visit Mars')
+			->assertSee('The red planet');
+	}
 }
