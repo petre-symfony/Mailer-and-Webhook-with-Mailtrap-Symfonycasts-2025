f0ac0fd7a5b90edf6b885751a4d7b9b2216a2d19
petrero <petrero@gmail.com>
2025-05-08 20:31:48 +0300

    17.6 testNoRemindersSent()  - The first test is easy: we want to ensure that, when there's no bookings to remind, the command doesn't send any emails. Write $this->executeConsoleCommand() and just the command name: app:send-booking-reminders. Ensure the command ran successfully with ->assertSuccessful() and ->assertOutputContains('Sent 0 booking reminders')
     testRemindersSent()
     - Arrange
     On to the next test! This one is more involved: we need to create a booking that is eligible for a reminder. Create the booking fixture with $booking = BookingFactory::createOne(). Pass an array with 'trip' => TripFactory::new(), and inside that, another array with 'name' => 'Visit Mars', 'slug' => 'iss' (to avoid the image issue). The booking also needs a customer: 'customer' => CustomerFactory::new(). All we care about is the customer's email: 'email' => 'steve@minecraft.com'. Finally, the booking date: 'date' => new \DateTimeImmutable('+4 days')
    
     Phew! We have a booking in the database that needs a reminder sent. This test's setup, or arrange step, is done.

--- tests/Functional/Command/SendBookingRemainderCommandTest.php
+++ tests/Functional/Command/SendBookingRemainderCommandTest.php
@@ -2,6 +2,9 @@
 
 namespace App\Tests\Functional\Command;
 
+use App\Factory\BookingFactory;
+use App\Factory\CustomerFactory;
+use App\Factory\TripFactory;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Zenstruck\Console\Test\InteractsWithConsole;
 use Zenstruck\Foundry\Test\Factories;
@@ -12,10 +15,23 @@ class SendBookingRemainderCommandTest extends KernelTestCase {
 	use ResetDatabase, Factories, InteractsWithMailer, InteractsWithConsole;
 
 	public function testNoReminderSent() {
-		$this->markTestIncomplete();
+		$this
+			->executeConsoleCommand('app:send-booking-reminders')
+			->assertSuccessful()
+			->assertOutputContains('Sent 0 booking reminders')
+		;
 	}
 
 	public function testReminderSent() {
-		$this->markTestIncomplete();
+		$booking = BookingFactory::createOne([
+			'trip' => TripFactory::new([
+				'name' => 'Visit Mars',
+				'slug' => 'iss'
+			]),
+			'customer' => CustomerFactory::new([
+				'email' => 'steve@minecraft.com'
+			]),
+			'date' => new \DateTimeImmutable('+4 days')
+		]);
 	}
 }
