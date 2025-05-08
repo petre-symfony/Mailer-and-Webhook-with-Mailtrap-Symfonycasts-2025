fccc99739dc2eb946e3aa80f50aeea4b402fd184
petrero <petrero@gmail.com>
2025-05-08 20:15:04 +0300

    16.4 The $io object has a trick for this. Write foreach ($io->progressIterate($bookings) as $booking). This handles all the boring progress bar logic for us! Inside, we need to create a new email. In TripController, copy that email - including these headers, and paste it here.  But we need to adjust this a bit: remove the attachment. And for the subject: replace "Confirmation" with "Reminder". Above, add some variables for convenience: $customer = $booking->getCustomer() and $trip = $booking->getTrip(). Down here, keep the same metadata, but change the tag to booking_reminder. This will help us better distinguish these emails in Mailtrap.
     Oh, and of course, change the template to booking_reminder.html.twig.
    
     Still in the loop, send the email with $this->mailer->send($email) and mark the booking as having the reminder sent with $booking->setReminderSentAt(new \DateTimeImmutable('now')).
    
     Perfect! Outside the loop, call $this->em->flush() to save the changes to the database. Finally, celebrate with $io->success(sprintf('Sent %d booking reminders', count($bookings))).
    
     Testing time! Pop over to your terminal. To be sure we have a booking that needs a reminder sent, reload the fixtures with:
    
      symfony console doctrine:fixture:load
     Now, run our new command!
    
      symfony console app:send-booking-reminders
     Nice, 1 reminder sent! And the output will impress our colleagues! Before we check Mailtrap, run the command again:
    
      symfony console app:send-booking-reminders
    "Sent 0 booking reminders". Perfect! Our logic to mark bookings as having reminders sent works!
    
     Now check Mailtrap... here it is! As expected, it looks super similar to our confirmation email but, it says "Coming soon! " here: it's using the new template.
    
     X-Tag and X-Metadata
     - When using "Mailtrap Testing", Mailer tags and metadata are not converted to Mailtrap categories and custom variables like they are when sent in production. But you can still make sure they're being sent! Click this "Tech Info" tab and scroll down a bit. When Mailer doesn't know how to convert tags and metadata, it adds them as these generic custom headers: X-Tag and X-Metadata.
    
     Sure enough, X-Tag is booking_reminder. Awesome, that's what we expect too!
    ?

--- src/Command/SendBookingRemindersCommand.php
+++ src/Command/SendBookingRemindersCommand.php
@@ -4,6 +4,7 @@ namespace App\Command;
 
 use App\Repository\BookingRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -11,7 +12,10 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Mailer\Header\MetadataHeader;
+use Symfony\Component\Mailer\Header\TagHeader;
 use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
 
 #[AsCommand(
 	name: 'app:send-booking-reminders',
@@ -34,9 +38,31 @@ class SendBookingRemindersCommand extends Command {
 		$bookings = $this->bookingRepo->findBookingsToRemind();
 
 		foreach ($io->progressIterate($bookings) as $booking) {
+			$customer = $booking->getCustomer();
+			$trip = $booking->getTrip();
+			$email = (new TemplatedEmail())
+				->to(new Address($customer->getEmail(), $customer->getName()))
+				->subject('Booking Reminder for '. $trip->getName())
+				->htmlTemplate('email/booking_reminder.html.twig')
+				->context([
+					'customer' => $customer,
+					'trip' => $trip,
+					'booking' => $booking
+				])
+			;
 
+			$email->getHeaders()->add(new TagHeader('booking reminder'));
+			$email->getHeaders()->add(new MetadataHeader('booking_uid', $booking->getUid()));
+			$email->getHeaders()->add(new MetadataHeader('customer_uid', $customer->getUid()));
+
+			$this->mailer->send($email);
+			$booking->setReminderSentAt(new \DateTimeImmutable('now'));
 		}
-		
+
+		$this->em->flush();
+
+		$io->success(sprintf('Sent %d booking reminders', count($bookings)));
+
 		return Command::SUCCESS;
 	}
 }
