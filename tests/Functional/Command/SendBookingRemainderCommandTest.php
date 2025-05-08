42c32ffd8f6c72aac40a84daea3d4deb3cd4869b
petrero <petrero@gmail.com>
2025-05-08 20:28:18 +0300

    17.5 To enable this package's helpers, add a new behavior trait to our test: InteractsWithConsole  We're ready to knock down those I's!

--- tests/Functional/Command/SendBookingRemainderCommandTest.php
+++ tests/Functional/Command/SendBookingRemainderCommandTest.php
@@ -3,12 +3,13 @@
 namespace App\Tests\Functional\Command;
 
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Zenstruck\Console\Test\InteractsWithConsole;
 use Zenstruck\Foundry\Test\Factories;
 use Zenstruck\Foundry\Test\ResetDatabase;
 use Zenstruck\Mailer\Test\InteractsWithMailer;
 
 class SendBookingRemainderCommandTest extends KernelTestCase {
-	use ResetDatabase, Factories, InteractsWithMailer;
+	use ResetDatabase, Factories, InteractsWithMailer, InteractsWithConsole;
 
 	public function testNoReminderSent() {
 		$this->markTestIncomplete();
