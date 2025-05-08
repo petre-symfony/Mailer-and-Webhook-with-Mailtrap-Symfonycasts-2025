9f5a95506959ceb92ded604a5e7cf9ee3aacf8a0
petrero <petrero@gmail.com>
2025-05-08 20:12:34 +0300

    16.3 Go check it out! src/Command/SendBookingRemindersCommand.php. Change the description to "Send booking reminder emails"  In the constructor, autowire & set properties for BookingRepository, EntityManagerInterface and MailerInterface
     This command doesn't need any arguments or options, so remove the configure() method entirely.
    
     Clear out the guts of execute(). Start by adding a nice: $io->title('Sending booking reminders'). Then, grab the bookings that need reminders sent, with $bookings = $this->bookingRepo->findBookingsToRemind().
    
     Easy Progress Bar
     - To be the absolute best, let's show a progress bar as we loop over the bookings. The $io object has a trick for this. Write foreach ($io->progressIterate($bookings) as $booking).

--- src/Command/SendBookingRemindersCommand.php
+++ src/Command/SendBookingRemindersCommand.php
@@ -2,6 +2,8 @@
 
 namespace App\Command;
 
+use App\Repository\BookingRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -9,36 +11,32 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Mailer\MailerInterface;
 
 #[AsCommand(
 	name: 'app:send-booking-reminders',
-	description: 'Add a short description for your command',
+	description: 'Send booking reminder emails',
 )]
 class SendBookingRemindersCommand extends Command {
-	public function __construct() {
+	public function __construct(
+		private BookingRepository $bookingRepo,
+		private EntityManagerInterface $em,
+		private MailerInterface $mailer
+	) {
 		parent::__construct();
-	}
-
-	protected function configure(): void {
-		$this
-			->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
-			->addOption('option1', null, InputOption::VALUE_NONE, 'Option description');
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output): int {
 		$io = new SymfonyStyle($input, $output);
-		$arg1 = $input->getArgument('arg1');
 
-		if ($arg1) {
-			$io->note(sprintf('You passed an argument: %s', $arg1));
+		$io->title('Sending Booking reminders');
+
+		$bookings = $this->bookingRepo->findBookingsToRemind();
+
+		foreach ($io->progressIterate($bookings) as $booking) {
+
 		}
-
-		if ($input->getOption('option1')) {
-			// ...
-		}
-
-		$io->success('You have a new command! Now make it your own! Pass --help to see your options.');
-
+		
 		return Command::SUCCESS;
 	}
 }
