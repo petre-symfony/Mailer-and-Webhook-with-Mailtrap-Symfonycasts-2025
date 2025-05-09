ed2f7f9693642f4754f5586f459c591634dfbe0d
petrero <petrero@gmail.com>
2025-05-09 18:24:32 +0300

    18.2 Jump to TripController::show(), copy all the email creation code, and paste it here. Up top, we need two variables: $customer = $booking->getCustomer() and $trip = $booking->getTrip(). Remove attachFromPath(), subject(), and htmlTemplate(). In this TagHeader, use the passed $tag variable. We can leave the metadata the same. Finally, return the $email

--- src/Email/BookingEmailFactory.php
+++ src/Email/BookingEmailFactory.php
@@ -5,6 +5,9 @@ namespace App\Email;
 use App\Entity\Booking;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\Mailer\Header\MetadataHeader;
+use Symfony\Component\Mailer\Header\TagHeader;
+use Symfony\Component\Mime\Address;
 
 class BookingEmailFactory {
 	public function __construct(
@@ -22,6 +25,21 @@ class BookingEmailFactory {
 	}
 
 	private function createEmail(Booking $booking, string $tag): TemplatedEmail {
-		
+		$customer = $booking->getCustomer();
+		$trip = $booking->getTrip();
+		$email = (new TemplatedEmail())
+			->to(new Address($customer->getEmail(), $customer->getName()))
+			->context([
+				'customer' => $customer,
+				'trip' => $trip,
+				'booking' => $booking
+			])
+		;
+
+		$email->getHeaders()->add(new TagHeader($tag));
+		$email->getHeaders()->add(new MetadataHeader('booking_uid', $booking->getUid()));
+		$email->getHeaders()->add(new MetadataHeader('customer_uid', $customer->getUid()));
+
+		return $email;
 	}
 }
